2bef8c6ef8300251dd0ecb0a76c758549cc94e54
Tracy <Tracy@atguigu.com>
2022-08-08 17:04:59 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,12 +1,10 @@
 package com.atguigu;
 
-/**
- * @author shkstart
- * @create 2022-08-08 16:54
- */
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
