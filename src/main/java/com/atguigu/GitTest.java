f478979454bd4ad0d5ab8a28378916ac2f8a31e1
Tracy <Tracy@atguigu.com>
2022-08-08 17:00:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author shkstart
+ * @create 2022-08-08 16:54
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
